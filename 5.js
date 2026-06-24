e589932b8996f91d9ccc9ed6cb5f1fd60f3d6b9d
dimastadeoo <dimastadeoo@gmail.com>
2026-06-24 17:37:52 +0700

    fix: description question in number 66

--- 5.js
+++ 5.js
@@ -1,37 +1,36 @@
 /*
- 66. Ditambah Satu
- Anda diberikan bilangan bulat besar yang direpresentasikan sebagai larik bilangan bulat digits, di mana setiap digit digits[i]adalah angka dari bilangan bulat tersebut. Angka-angka tersebut diurutkan dari yang paling signifikan ke yang paling tidak signifikan dari kiri ke kanan. Bilangan bulat besar tersebut tidak mengandung angka nol di depannya.ith0
+66. Plus One
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
-Tambahkan satu pada bilangan bulat besar dan kembalikan array angka yang dihasilkan .
+Increment the large integer by one and return the resulting array of digits.
 
-Contoh 1:
+Example 1:
 
 Input: digits = [1,2,3]
- Output: [1,2,4]
- Penjelasan: Array tersebut mewakili bilangan bulat 123.
-Menambah satu menjadi 123 + 1 = 124.
-Dengan demikian, hasilnya seharusnya [1,2,4].
-Contoh 2:
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+Example 2:
 
 Input: digits = [4,3,2,1]
- Output: [4,3,2,2]
- Penjelasan: Array tersebut mewakili bilangan bulat 4321.
-Menambah satu menjadi 4321 + 1 = 4322.
-Dengan demikian, hasilnya seharusnya [4,3,2,2].
-Contoh 3:
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+Example 3:
 
 Input: digits = [9]
- Output: [1,0]
- Penjelasan: Array tersebut mewakili bilangan bulat 9.
-Menambah satu menjadi 9 + 1 = 10.
-Dengan demikian, hasilnya seharusnya [1,0].
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
  
-
-Batasan:
+Constraints:
 
 1 <= digits.length <= 100
 0 <= digits[i] <= 9
-digitstidak mengandung awalan 0's.
+digits does not contain any leading 0's.
  */
 
 /**
